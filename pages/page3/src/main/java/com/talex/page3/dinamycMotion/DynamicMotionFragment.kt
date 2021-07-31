ea66e155f775cc6c6adb531b3a9e3757c9c938f0
alex <alexander.topilskii@gmail.com>
2021-07-31 11:58:42 +0300

    some awesome

--- pages/page3/src/main/java/com/talex/page3/dinamycMotion/DynamicMotionFragment.kt
+++ pages/page3/src/main/java/com/talex/page3/dinamycMotion/DynamicMotionFragment.kt
@@ -1,6 +1,12 @@
 package com.talex.page3.dinamycMotion
 
 import android.content.Context
+import android.os.Bundle
+import android.view.View
+import androidx.constraintlayout.motion.widget.MotionLayout
+import androidx.constraintlayout.motion.widget.MotionScene
+import androidx.constraintlayout.motion.widget.TransitionBuilder
+import androidx.constraintlayout.widget.ConstraintSet
 import androidx.lifecycle.ViewModelProvider
 import androidx.fragment.app.Fragment
 import com.talex.page3.R
@@ -16,7 +22,34 @@ class DynamicMotionFragment : Fragment(R.layout.page3_dynamic_motion_fragment) {
     override fun onAttach(context: Context) {
         super.onAttach(context)
         viewModel = ViewModelProvider(this).get(DynamicMotionViewModel::class.java)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val motionLayout = view.findViewById<MotionLayout>(R.id.page3_dynamic_motion_fragment_scene__container)
+
 
     }
+
+    private fun MotionLayout.createTransition(scene: MotionScene): MotionScene.Transition {
+        val startSetId = View.generateViewId()
+        val startSet = ConstraintSet()
+        startSet.clone(this)
+
+        val endSetId = View.generateViewId()
+        val endSet = ConstraintSet()
+        endSet.clone(this)
+
+        val transitionId = View.generateViewId()
+        return TransitionBuilder.buildTransition(
+            scene,
+            transitionId,
+            startSetId,
+            startSet,
+            endSetId,
+            endSet
+        )
+    }
+
 
 }
